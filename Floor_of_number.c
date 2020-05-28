f73437beee2dba58a52a169675c0f9dcb275f0b6
Sidharth Puri <52946626+sidharth-puri@users.noreply.github.com>
2020-05-28 12:07:44 +0530

    Create Floor_of_number.c

---
+++ Floor_of_number.c
@@ -0,0 +1,44 @@
+#include<stdio.h> 
+
+int floorSearch(int arr[], int l, int r, int x) 
+{ 
+  int ans=-1;
+ while (l <= r) { 
+        int m = l + (r - l) / 2; 
+  
+       
+        if (arr[m] == x) 
+            return m; 
+  
+   
+        if (arr[m] < x) {
+           ans=m;
+            l = m + 1; 
+        }
+  
+     
+        else {
+         
+            r = m - 1;
+          
+        }
+    } 
+  
+    
+    return ans; 
+} 
+
+
+int main() 
+{ 
+int arr[] = {1, 2, 8, 10, 10, 12, 19}; 
+int n = sizeof(arr)/sizeof(arr[0]); 
+int x = 18; 
+int index = floorSearch(arr, 0, n-1, x); 
+if(index == -1) 
+	printf("Floor of %d doesn't exist in array ", x); 
+else
+	printf("floor of %d is %d", x,arr[ index]); 
+getchar(); 
+return 0; 
+} 
