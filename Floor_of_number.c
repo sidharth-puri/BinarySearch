67b75790b8f2b22751d996770701ed6c857f975e
Sidharth Puri <52946626+sidharth-puri@users.noreply.github.com>
2020-05-28 12:08:24 +0530

    Update Floor_of_number.c

--- Floor_of_number.c
+++ Floor_of_number.c
@@ -5,25 +5,21 @@ int floorSearch(int arr[], int l, int r, int x)
   int ans=-1;
  while (l <= r) { 
         int m = l + (r - l) / 2; 
-  
-       
+
         if (arr[m] == x) 
-            return m; 
-  
+            return m;  
    
         if (arr[m] < x) {
            ans=m;
             l = m + 1; 
         }
   
-     
         else {
          
             r = m - 1;
           
         }
     } 
-  
     
     return ans; 
 } 
